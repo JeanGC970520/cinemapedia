2d3db982a0baf6c1d66ed26cc539338fcbcc3038
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-19 17:19:50 -0600

    Using mapper. Filter movies that doesn't have poster

--- lib/infrastructure/datasources/the_moviedb_datasource.dart
+++ lib/infrastructure/datasources/the_moviedb_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -18,7 +20,15 @@ class TheMovieDbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPLaying({int page = 1}) async {
 
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+
+    final movieDBResponse = MovieDbResponse.fromMap(response.data);
+    // the where() method filters the moviedb where it has no poster 
+    // and doesn't use it to pass it to the map() method 
+    final List<Movie> movies = movieDBResponse.results
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+    ).toList();
 
     return movies;
   }
