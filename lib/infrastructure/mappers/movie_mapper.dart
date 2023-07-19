5df2d11a4a11b9040047861f4ae2b73b937babc0
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-19 17:20:37 -0600

    Change default value on posterPath

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -9,7 +9,7 @@ class MovieMapper {
 
   static Movie movieDBToEntity( MovieMovieDB moviedb ) => Movie(
     adult: moviedb.adult, 
-    backdropPath: moviedb.backdropPath != '' 
+    backdropPath: (moviedb.backdropPath != '') 
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
       : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png', 
     genreIds: moviedb.genreIds.map((e) => e.toString()).toList(), 
@@ -18,9 +18,9 @@ class MovieMapper {
     originalTitle: moviedb.originalTitle, 
     overview: moviedb.overview, 
     popularity: moviedb.popularity, 
-    posterPath: moviedb.posterPath != '' 
+    posterPath: (moviedb.posterPath != '') 
       ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
-      : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png', 
+      : 'no-poster', 
     releaseDate: moviedb.releaseDate, 
     title: moviedb.title, 
     video: moviedb.video, 
