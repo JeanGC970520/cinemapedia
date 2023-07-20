e869616fab96f06f429845d260882ce93f4dfd35
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 16:38:24 -0600

    Adding notes

--- lib/presentation/providers/movies/initial_loading_provider.dart
+++ lib/presentation/providers/movies/initial_loading_provider.dart
@@ -6,6 +6,8 @@ import 'movies_providers.dart';
 
 final initialLoadingProvider = Provider<bool>((ref) {
   
+  // With the watch method, we can listen changes on the providers
+  // and with that request if the data is empty or not and provide this info
   final step1 = ref.watch( nowPlayingMoviesProvider ).isEmpty;
   final step2 = ref.watch( popularMoviesProvider ).isEmpty;
   final step3 = ref.watch( topRatedMoviesProvider ).isEmpty;
