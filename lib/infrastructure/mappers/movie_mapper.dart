a4549703083f860663e1e7a1335de7d7098fafd5
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 17:57:10 -0600

    Adding method to mapping from movie data fetching by id to an instance of my Entity

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -3,6 +3,7 @@
 // a model to a entity. That is to say, convert data from my
 // datasource to my structure data defining on my rules(entity).
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -27,5 +28,27 @@ class MovieMapper {
     voteAverage: moviedb.voteAverage, 
     voteCount: moviedb.voteCount,
   ); 
+
+  // Mapper to pass the movie details fetching by id to mi Entity
+  static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+    adult: moviedb.adult, 
+    backdropPath: (moviedb.backdropPath != '') 
+      ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+      : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png', 
+    genreIds: moviedb.genres.map((e) => e.name).toList(), 
+    id: moviedb.id, 
+    originalLanguage: moviedb.originalLanguage, 
+    originalTitle: moviedb.originalTitle, 
+    overview: moviedb.overview, 
+    popularity: moviedb.popularity, 
+    posterPath: (moviedb.posterPath != '') 
+      ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+      : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png', 
+    releaseDate: moviedb.releaseDate, 
+    title: moviedb.title, 
+    video: moviedb.video, 
+    voteAverage: moviedb.voteAverage, 
+    voteCount: moviedb.voteCount,
+  );
 
 }
