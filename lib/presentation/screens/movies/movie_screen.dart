ff77dfe2fad027b328172e6ac890a882ef26edd7
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 17:09:53 -0600

    Screen to display movie information, using its id

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+
+  static const name = 'movie-screen';
+
+  const MovieScreen({
+    super.key,
+    required this.movieId,
+  });
+
+  final String movieId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MovieID: $movieId'),
+      ),
+    );
+  }
+}
