8e70f8155a74620c600307feca4cf0af40344b40
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 15:51:14 -0600

    Defining signature from upcoming and top rated movies

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -6,5 +6,9 @@ abstract class MoviesDatasource {
   Future<List<Movie>> getNowPLaying({ int page = 1 });
 
   Future<List<Movie>> getPopular({ int page = 1 });
+  
+  Future<List<Movie>> getUpcoming({ int page = 1 });
+  
+  Future<List<Movie>> getTopRated({ int page = 1 });
 
 }
