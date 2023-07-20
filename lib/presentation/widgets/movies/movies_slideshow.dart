c5cf0f41904da77b5d1826e2c7080d99d6889fcb
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 11:26:58 -0600

    Building card slide to display movie poster

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -37,6 +38,40 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 10),
+        ),
+      ]     
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              // Check if the image is loading
+              if( loadingProgress != null ) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration( color: Colors.black12 )
+                );
+              }
+
+              return FadeIn(child: child);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
