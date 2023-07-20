b4fcd17ccef701d1361004284334e0d527ffd98c
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 16:22:01 -0600

    Implement a loader with stream to display different messages

---
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+  static const messages = <String>[
+    'Cargando peliculas',
+    'Comprando palomitas de maiz',
+    'Cargando populares',
+    'Llamando a mi novia',
+    'Ya mero...',
+    'Esto esta tardando mas de lo esperado :(',
+  ];
+
+  Stream<String> getLoadingMessages() {
+    return Stream.periodic( const Duration(milliseconds: 1200), (step) {
+      return messages[step];
+    }).take(messages.length);
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Espere por favor'),
+          const SizedBox(height: 10,),
+          const CircularProgressIndicator(strokeWidth: 2.0,),
+          const SizedBox(height: 10,),
+
+          StreamBuilder(
+            stream: getLoadingMessages(),
+            builder: (context, snapshot) {
+              if( !snapshot.hasData ) return const Text('Loading...');
+
+              return Text( snapshot.data! );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
