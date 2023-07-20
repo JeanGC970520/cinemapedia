d0d3c354c66f2e3a2b4f30b87c4d4af8ecbb4dfb
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 16:01:01 -0600

    Implementing providers to manage upcoming and top rated movies

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -26,6 +26,26 @@ final popularMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>
   );
 });
 
+final topRatedMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  
+  final fetchMoreMovies = ref.watch( movieRepositoryProvider ).getTopRated;
+  
+  return MoviesNotifier(
+    fetchMoreMovies: fetchMoreMovies
+  );
+});
+
+final upcomingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  
+  final fetchMoreMovies = ref.watch( movieRepositoryProvider ).getUpcoming;
+  
+  return MoviesNotifier(
+    fetchMoreMovies: fetchMoreMovies
+  );
+});
+
+
+
 // Defining function signature
 typedef MovieCallback = Future<List<Movie>> Function({ int page });
 
