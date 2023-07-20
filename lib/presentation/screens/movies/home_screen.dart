de224e1e8b5b2891f0b910514298278733276f3c
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-19 20:30:44 -0600

    Using Stateful Widget converted to ConsumerStatefulWidget to use Riverpord providers

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,8 @@
 
 
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,7 +12,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    // Accesing to the notifier(controller) of the StateNotifierProvider
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Accesing to the state of the StateNotifierProvider and listen changes
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (BuildContext context, int index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
     );
   }
 }
