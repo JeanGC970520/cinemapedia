c98c9780385274565e35b77430ba283192be8f98
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 14:28:23 -0600

    Avoid many requests to call the next movie page

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -22,6 +22,7 @@ typedef MovieCallback = Future<List<Movie>> Function({ int page });
 class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
   
   MoviesNotifier({
@@ -29,9 +30,17 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
   }) : super([]);
 
   Future<void> loadNextPage() async {
+    if( isLoading ) return;
+
+    isLoading = true;
+    // print('LOad movies');
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    // This delayed only is used to await movies rendering 
+    // and prevent a second request during it
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
   }
 
 }
