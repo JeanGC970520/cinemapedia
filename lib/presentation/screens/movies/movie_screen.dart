24cd6c9cefcbe312ecfde074bcef5aaca9ba101d
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 16:13:59 -0600

    Adding animation to sliver app bar and actor image

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../domain/entities/movie.dart';
@@ -169,13 +170,15 @@ class _ActorsByMovie extends ConsumerWidget {
               children: [
 
                 // Actor photo
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    actor.profilePath,
-                    height: 180,
-                    width: 135,
-                    fit: BoxFit.cover,
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
 
@@ -230,6 +233,13 @@ class CustomSliverAppBar extends StatelessWidget {
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  
+                  if( loadingProgress != null ) return const SizedBox();
+
+                  return FadeIn(child: child);
+
+                },
               ),
             ),
 
