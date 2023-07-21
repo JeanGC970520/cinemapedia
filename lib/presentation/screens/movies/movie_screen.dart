2bb004be9843f534fed97d322aab3fd64ddec4ba
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 13:35:21 -0600

    Adding movie details

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -48,11 +48,94 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
           CustomSliverAppBar( movie: movie, ),
 
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDetails(movie: movie),
+              childCount: 1,
+            )
+          ),
+
         ],
       ),
     );
   }
 }
+
+class _MovieDetails extends StatelessWidget {
+  const _MovieDetails({
+    required this.movie,
+  });
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+    
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              // Image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+
+              const SizedBox(width: 10,),
+
+              // Description
+              SizedBox(
+                width: (size.width * 0.7) - 40,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(movie.title, style: textStyles.titleLarge,),
+                    Text(movie.overview,),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+
+        // Movie genres
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((genre) => Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Chip(
+                  label: Text(genre),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                ),
+              ))
+            ],
+          ),
+        ),
+
+        // TODO: Display actors on Listview
+        
+        const SizedBox(height: 100,),
+      ],
+    );
+  }
+}
+
+
 class CustomSliverAppBar extends StatelessWidget {
   const CustomSliverAppBar({
     super.key,
