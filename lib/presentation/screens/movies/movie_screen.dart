b8b6f8ef92bbb62c88f2a41c75dbb480d48d9148
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-20 20:27:46 -0600

    Using a ConsumerStatefulWidget to request and display movie info

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,8 +1,12 @@
 
-
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+
+class MovieScreen extends ConsumerStatefulWidget {
 
   static const name = 'movie-screen';
 
@@ -14,10 +18,32 @@ class MovieScreen extends StatelessWidget {
   final String movieId;
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Calling method to request movie by id if it is not in cache
+    ref.read( movieInfoProvider.notifier ).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    final Movie? movie = ref.watch( movieInfoProvider )[widget.movieId];
+
+    if( movie == null ) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator(strokeWidth: 2.0,),)
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieId'),
+        title: Text('MovieID: ${widget.movieId}'),
       ),
     );
   }
