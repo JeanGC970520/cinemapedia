ee4f1611407d4240c27a6a50a64ca4e63746b699
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 15:24:12 -0600

    Creating only-read provider to manage my actors repository implementation

---
+++ lib/presentation/providers/actors/actors_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/infrastructure/datasources/actor_moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/respositories/actor_repository_impl.dart';
+
+final actorsRepositoryProvider = Provider(
+  (ref) => ActorRepositoryImpl( ActorMovieDbDatasource() )
+); 
