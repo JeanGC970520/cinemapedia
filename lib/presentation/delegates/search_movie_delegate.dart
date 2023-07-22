393489aaf27068667a64f6e4bae1126ddfe9dd93
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 19:39:12 -0600

    Implementing Widget to display search item content

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
 
 import '../../domain/entities/movie.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function( String query, { int page } ); 
 
@@ -61,14 +62,78 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         return ListView.builder(
           itemCount: movies.length,
           itemBuilder: (BuildContext context, int index) {
-            final movie = movies[index];
-            return ListTile(
-              title: Text(movie.title),
-            );
+            return _MovieSearchItem(movie: movies[index]);
           },
         );
       },
     );
   }
 
+}
+
+class _MovieSearchItem extends StatelessWidget {
+  const _MovieSearchItem({
+    required this.movie,
+  });
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+
+          // Image
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10,),
+
+          // Description
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+          
+                Text(movie.title, style: textStyles.titleMedium,),
+
+                Text(
+                  movie.overview, 
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                    const SizedBox(width: 5,),
+                    Text(
+                      HumanFortmats.number(movie.voteAverage, 1),
+                      style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade900),
+                    ),
+                  ],
+                ),
+          
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
 }
