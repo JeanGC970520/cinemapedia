979b0faa18c8791b3214784d7e496fe475146dec
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-22 14:28:30 -0600

    Implementing a StateNotifierProvider to mantein the searched movies and request them too

--- lib/presentation/providers/search/search_movies_providers.dart
+++ lib/presentation/providers/search/search_movies_providers.dart
@@ -2,5 +2,42 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../domain/entities/movie.dart';
+import '../movies/movies_repository_provider.dart';
+
 final searchQueryProvider = StateProvider<String>((ref) => '');
+
+
+final searchedMoviesProvider = StateNotifierProvider<SearchedMoviesNotifier, List<Movie>>((ref) {
+
+  final movieRepository = ref.watch( movieRepositoryProvider );
+
+  return SearchedMoviesNotifier(
+    searchMovies: movieRepository.searchMovies, 
+    ref: ref
+  );
+}); 
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function( String query, { int page });
+
+class SearchedMoviesNotifier extends StateNotifier<List<Movie>> {
+  
+  SearchedMoviesNotifier({
+    required this.searchMovies,
+    required this.ref,
+  }): super([]);
+
+  final SearchMoviesCallback searchMovies;
+  final Ref ref;
+
+  Future<List<Movie>> searchMoviesByQuery( String query, { int page = 1 } ) async {
+
+    final List<Movie> movies = await searchMovies(query, page: page);
+    ref.read( searchQueryProvider.notifier ).update((state) => query);
+
+    state = movies;
+    return movies;
+  }
+
+}
 
