92ffe45a67821d59fb96fefc34ff371645019707
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 19:57:04 -0600

    Implementing GestureDetector and receiving the method to execute when the onTap event is triggered

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -62,7 +62,10 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         return ListView.builder(
           itemCount: movies.length,
           itemBuilder: (BuildContext context, int index) {
-            return _MovieSearchItem(movie: movies[index]);
+            return _MovieSearchItem(
+              movie: movies[index],
+              onMovieSelected: close,
+            );
           },
         );
       },
@@ -74,9 +77,11 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 class _MovieSearchItem extends StatelessWidget {
   const _MovieSearchItem({
     required this.movie,
+    required this.onMovieSelected,
   });
 
   final Movie movie;
+  final Function onMovieSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -84,55 +89,58 @@ class _MovieSearchItem extends StatelessWidget {
     final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-
-          // Image
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                movie.posterPath,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => onMovieSelected(context, movie),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+    
+            // Image
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 10,),
-
-          // Description
-          SizedBox(
-            width: size.width * 0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-          
-                Text(movie.title, style: textStyles.titleMedium,),
-
-                Text(
-                  movie.overview, 
-                  maxLines: 5,
-                  overflow: TextOverflow.ellipsis,
-                ),
-
-                Row(
-                  children: [
-                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
-                    const SizedBox(width: 5,),
-                    Text(
-                      HumanFortmats.number(movie.voteAverage, 1),
-                      style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade900),
-                    ),
-                  ],
-                ),
-          
-              ],
-            ),
-          )
-
-        ],
+            const SizedBox(width: 10,),
+    
+            // Description
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+            
+                  Text(movie.title, style: textStyles.titleMedium,),
+    
+                  Text(
+                    movie.overview, 
+                    maxLines: 5,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+    
+                  Row(
+                    children: [
+                      Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                      const SizedBox(width: 5,),
+                      Text(
+                        HumanFortmats.number(movie.voteAverage, 1),
+                        style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade900),
+                      ),
+                    ],
+                  ),
+            
+                ],
+              ),
+            )
+    
+          ],
+        ),
       ),
     );
   }
