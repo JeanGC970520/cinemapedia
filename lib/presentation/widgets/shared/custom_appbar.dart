ede32eba1b9f4b1440bb92f36e7bdd7d6bd11a0a
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-21 18:01:24 -0600

    Convert StatelessWidget to ConsumerWidget and use movie repository provider

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,14 +1,17 @@
 
 import 'package:flutter/material.dart';
 
-import '../../delegates/search_movie_delegate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+import '../../delegates/search_movie_delegate.dart';
+import '../../providers/movies/movies_repository_provider.dart';
+
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
@@ -31,9 +34,13 @@ class CustomAppbar extends StatelessWidget {
               IconButton(
                 onPressed: () {
 
+                  final movieRepository = ref.read( movieRepositoryProvider );
+
                   showSearch(
                     context: context, 
-                    delegate: SearchMovieDelegate(),
+                    delegate: SearchMovieDelegate(
+                      searchMovies: movieRepository.searchMovies,
+                    ),
                   );
 
                 },
