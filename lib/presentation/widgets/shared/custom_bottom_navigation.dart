0e7e5ff72e1d3e931e4b9d61b89e02eda7b1a830
Jean <jeanpoolgarcia_97@hotmail.com>
2023-07-23 14:28:52 -0600

    Adding property to recive the current index and on the onTap navigate to home with an route argument

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,13 +1,23 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  const CustomBottomNavigation({
+    super.key,
+    required this.currentIndex,
+  });
+
+  final int currentIndex;
+
+
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (selectedIndex) => context.go('/home/$selectedIndex'),
       elevation: 0,
       items: const [
 
