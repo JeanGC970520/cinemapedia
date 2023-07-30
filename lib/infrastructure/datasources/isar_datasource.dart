58ef8f339e66659ee0d264fa09c2dcdbe16c51a4
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-07-29 18:31:44 -0600

    Implenting a open method to instantiate the Isar DB

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,20 +1,44 @@
 
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
+
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+
+    final dir = await getApplicationDocumentsDirectory();
+
+    if( Isar.instanceNames.isEmpty ) {
+      return await Isar.open(
+        [ MovieSchema ],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isFavoriteMovie(int movieId) {
     throw UnimplementedError();
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
+  Future<void> toggleFavorite(Movie movie) {
     throw UnimplementedError();
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
+  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
     throw UnimplementedError();
   }
 
