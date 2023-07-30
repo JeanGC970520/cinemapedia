03f04f180c8a32947ef55a793e8e382d3f801526
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-07-30 15:40:52 -0600

    StateNotifierProvider to manage the state of the favorite movies and display it

---
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../domain/entities/movie.dart';
+import '../../../domain/repositories/local_storage_repository.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+
+final favoriteMoviesProvider = StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+  final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+  return StorageMoviesNotifier(localStorageRepository: localStorageRepository);
+});
+
+
+/* Data structure
+
+  {
+    1234 : Movie,
+    2344 : Movie,
+    2341 : Movie,
+  }
+
+*/
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+
+  StorageMoviesNotifier({
+    required this.localStorageRepository,
+  }): super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(offset: page * 10,); // TODO: Limit 20
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for( final movie in movies ) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = { ...state, ...tempMoviesMap };
+
+    // return movies;
+  }
+
+}
