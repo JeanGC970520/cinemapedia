47a5896d6096c8f43a0a1dca759941b127488c86
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-07-30 18:24:07 -0600

    Implementing a ConsumerStatefulWidget and method to use provider that manage favorite movies

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,57 @@
 
-
 import 'package:flutter/material.dart';
 
-class FavoritesView extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+import '../../widgets/widgets.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  bool isLoading = false;
+  bool isLastPage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+
+    if( isLoading || isLastPage ) return; 
+    isLoading = true;
+    
+    // Load the favorite movies of any datasource that has used
+    final movies = await ref.read( favoriteMoviesProvider.notifier ).loadNextPage();
+    isLoading = false;
+
+    if( movies.isEmpty ) {
+      isLastPage = true;
+    }
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    final favoriteMoviesMap = ref.watch( favoriteMoviesProvider );
+    final favoriteMovies = favoriteMoviesMap.values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
+      body: MovieMasonry(
+        movies: favoriteMovies, 
+        loadNextPage: loadNextPage,
       ),
-      body: const Center(child: Text('Favorites'),),
     );
   }
 }
